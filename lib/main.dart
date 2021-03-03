86ff8db27d0a13874cb94f9da4a424ebdbdc436b
alpacawool <44758340+alpacawool@users.noreply.github.com>
2021-03-02 16:37:13 -0800

    Added Firebase dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 
-void main() {
+void main() async{
+   // Needed to prevent exception with screen rotation
+  WidgetsFlutterBinding.ensureInitialized();
+   // Handle screen rotation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitUp
+  ]);
+  // Needed for firebase storage
+  await Firebase.initializeApp();
   runApp(App());
 }
